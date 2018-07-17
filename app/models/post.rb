5da207945ee2c4b90e46d2d118f05e5efe496f41
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 07:50:40 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,8 @@ class Post < ActiveRecord::Base
     bait.each do |b|
       if self.title.nil? || !self.title.match(b)
         errors.add(:title, "clickbait")
+      else
+        return true
       end
     end
   end
