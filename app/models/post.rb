de568cebf413489df552d68db618618d03c783da
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 07:01:39 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 150 }
   validates :summary, length: { maximum: 250 }
+  validates :category,
+  validate :is_clickbait?
 
 end
