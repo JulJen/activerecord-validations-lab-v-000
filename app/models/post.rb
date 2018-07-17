2fd9df666224318f2e68734bfe88de5360e4992f
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 07:13:14 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,7 +7,7 @@ class Post < ActiveRecord::Base
 
   def is_clickbait?
     clickbait = [/Won't Believe/, /Secret/, /Top/, /Guess/]
-    if Title.match(clickbait)
+    if self.title.match(clickbait)
       errors.add(:title, "Looks like clickbait")
     end
   end
