e1df52d3a7f92adbfa0c8c8bd56630c05c9e474c
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 07:19:17 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,13 @@ class Post < ActiveRecord::Base
   validates :content, length: { minimum: 150 }
   validates :summary, length: { maximum: 250 }
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
-  validate :is_clickbait?
-
-  def is_clickbait?
-    clickbait = [/Won't Believe/, /Secret/, /Top/, /Guess/]
-    clickbait.each { |clickbait| !self.title.match(clickbait) ?: errors.add(:title, "Looks like clickbait"))
-  end
+  # validate :is_clickbait?
+  #
+  # def is_clickbait?
+  #   clickbait = [/Won't Believe/, /Secret/, /Top/, /Guess/]
+  #   clickbait.each do {|clickbait|}
+  #    !self.title.match(clickbait) ?: errors.add(:title, "Looks like clickbait"))
+  # end
   # end
 
 end
