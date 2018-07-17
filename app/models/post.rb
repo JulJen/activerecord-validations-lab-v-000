70550f952569edc59168841b056a36597940eb4c
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 07:05:27 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,12 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 150 }
   validates :summary, length: { maximum: 250 }
-  validates :category, presence: true
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validate :is_clickbait?
 
   def is_clickbait?
     clickbait = [/Won't Believe/, /Secret/, /Top/, /Guess/]
+    if clickbait.empty? { |}
   end
 
 end
