79f439e7935068ffff1c78c4755d93aa3d19c17f
Jul Jen <juliajen.prog@gmail.com>
2018-07-17 07:50:15 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,10 +5,11 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
   validate :is_clickbait?
 
+#add a custom validator to Post that ensures the title is sufficiently clickbait-y
   def is_clickbait?
     bait = [/Won't Believe/, /Secret/, /Top/, /Guess/]
     bait.each do |b|
-      if self.title.nil? || !self.title.match(b) 
+      if self.title.nil? || !self.title.match(b)
         errors.add(:title, "clickbait")
     end
   end
